fce76e16ff316023fc1b5255cadf1ad18744cfca
SUDENAZ KILIÇ <120420389+sudenaz44@users.noreply.github.com>
2024-07-30 15:59:40 +0300

    Update main.py

--- Hangman.py/main.py
+++ Hangman.py/main.py
@@ -1,4 +1,5 @@
 import random
+from replit import clear
 
 from hangman_words import word_list
 
@@ -20,6 +21,7 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    clear()
 
     if guess in display:
         print(f"You've already guessed {guess}")
